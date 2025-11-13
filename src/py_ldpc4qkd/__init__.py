2efc8cd15da40b3586d49d77c537494c5e526cc2
adomasbaliuka <52975890+adomasbaliuka@users.noreply.github.com>
2025-11-13 14:48:41 +0100

    Adds __version__ to __init__.py

--- src/py_ldpc4qkd/__init__.py
+++ src/py_ldpc4qkd/__init__.py
@@ -1,3 +1,10 @@
 from __future__ import annotations
 
 from ._core import *
+
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    __version__ = "unknown"
