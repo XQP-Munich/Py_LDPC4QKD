487bd95856352ca12f8297d9b389b38593d79fac
adomasbaliuka <52975890+adomasbaliuka@users.noreply.github.com>
2025-11-13 14:55:53 +0100

    CI: investigate windows python3.10

--- examples/tmp.py
+++ examples/tmp.py
@@ -5,3 +5,13 @@ print(ldpc.__version__)
 
 import numpy as np
 print(np.__version__)
+
+
+def print_available_codes():
+    for i in range(1_000_000):
+        try:
+            code = ldpc.get_rate_adaptive_code(i)
+            print(f"Code {i} maps {code.getNCols()} -> {code.get_n_rows_mother_matrix()}")
+        except RuntimeError as e:  # `get_rate_adaptive_code` throws `RuntimeError` if no code availabel for ID.
+            break
+    print("\n")
