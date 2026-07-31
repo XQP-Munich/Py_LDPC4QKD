a05a3ccce253f8911ba196f61c80d3724c86857a
adomasbaliuka <52975890+adomasbaliuka@users.noreply.github.com>
2026-07-31 14:36:42 +0200

    Improvements to examples

--- examples/basic.py
+++ examples/basic.py
@@ -1,4 +1,6 @@
+import warnings
 import math
+
 import numpy as np
 import py_ldpc4qkd as ldpc
 
@@ -17,16 +19,6 @@ def hash_vector(vec):
     return seed
 
 
-def print_available_codes():
-    for i in range(1_000_000):
-        try:
-            code = ldpc.get_rate_adaptive_code(i)
-            print(f"Code {i} maps {code.getNCols()} -> {code.get_n_rows_mother_matrix()}")
-        except RuntimeError as e:  # `get_rate_adaptive_code` throws `RuntimeError` if no code availabel for ID.
-            break
-    print("\n")
-
-
 def test_small():
     """
     How to use LDPC code with all manual settings
@@ -41,8 +33,8 @@ def test_small():
     syndrome = code.encode_no_ra(key)
     print(f"{syndrome=}")
 
-    ch_param = 1 / 7
-    vlog = np.log((1 - ch_param) / ch_param)
+    qber = 1 / 7
+    vlog = np.log((1 - qber) / qber)
     llrs = np.array([vlog * (1. - 2. * noisy_bit) for noisy_bit in noisy_key], dtype=np.double)
 
     out = np.zeros(len(key), dtype=np.uint8)
@@ -68,14 +60,24 @@ def test_small_default():
     syndrome = code.encode_no_ra(key)
     print(f"{syndrome=}")
 
-    ch_param = 1 / 7
+    qber = 1 / 7
     out = np.zeros(len(key), dtype=np.uint8)
-    is_decoding_success: bool = code.decode_default(noisy_key, syndrome, out, ch_param)
+    is_decoding_success: bool = code.decode_default(noisy_key, syndrome, out, qber)
 
     assert is_decoding_success, "Decoder did not converge!"
     assert np.all(out == key), "Decoder converged to wrong codeword!"
     print(f"{out=}")
     print("SUCCESS!\n\n")
+
+
+def print_available_codes():
+    for i in range(1_000_000):
+        try:
+            code = ldpc.get_rate_adaptive_code(i)
+            print(f"Code {i} maps {code.getNCols()} -> {code.get_n_rows_mother_matrix()}")
+        except RuntimeError as e:  # `get_rate_adaptive_code` throws `RuntimeError` if no code availabel for ID.
+            print("\n")
+            return i
 
 
 def test_encode_with_ra():
@@ -96,6 +98,7 @@ def test_encode_with_ra():
     is_decoding_success: bool = code.decode_default(noisy_key, syndrome, corrected_noisy_key, ch_param)
     assert is_decoding_success, "Decoder did not converge!"
     assert np.all(corrected_noisy_key == key), "Decoder converged to wrong codeword!"
+
 
 
 def get_test_key(size):
@@ -131,11 +134,11 @@ def test_print_version():
     print(ldpc.__version__)
 
 
-def test_big():
+def test_big(code_id):
     """
     Very very crude frame error rate simulation
     """
-    code = ldpc.get_rate_adaptive_code(1)
+    code = ldpc.get_rate_adaptive_code(code_id)
 
     n_trials = 100
     n_failures = 0
@@ -149,29 +152,29 @@ def test_big():
     # assert hash_vector(syndrome) == 2814594723, \
     #     "value (from C++ code) for small matrix 6144 -> 2048 and input hash 1233938212"
 
-    true_target_ch_param = 0.05
+    true_target_qber = 0.05
     for sim_idx in range(n_trials):
-        noisy_key = binary_symmetric_channel(key, true_target_ch_param)
+        noisy_key = binary_symmetric_channel(key, true_target_qber)
         # print(f"{hash_vector(noisy_key)=}")
         # print(f"errors: {len(noisy_key) - np.count_nonzero(noisy_key == key)} "
         #       f"({(len(noisy_key) - np.count_nonzero(noisy_key == key)) / len(noisy_key):.2%})")
         if sim_idx % (3_000_000 // code.getNCols()) == 1:
-            print(f"\rSimulating frame error rate at BSC({true_target_ch_param:.2%}) ({sim_idx} / {n_trials}, "
+            print(f"\rSimulating frame error rate at BSC({true_target_qber:.2%}) ({sim_idx} / {n_trials}, "
                   f"current FER ~ {n_failures / sim_idx:.2E})", end="")
-        estim_ch_param = 0.05
+        estim_qber = 0.05
         corrected_noisy_key = np.zeros(len(key), dtype=np.uint8)
-        is_decoding_success: bool = code.decode_default(noisy_key, syndrome, corrected_noisy_key, estim_ch_param)
+        is_decoding_success: bool = code.decode_default(noisy_key, syndrome, corrected_noisy_key, estim_qber)
         if not np.all(key == corrected_noisy_key):
             n_failures += 1
             if is_decoding_success:
                 print("Reported decoding success despite incorrect result! (this should be very rare!)")
         elif not is_decoding_success:
             print("Reported decoding failure despite CORRECT RESULT. (This has to be a bug!)")
-    print(f"\rSimulation done. Did {n_trials=} and {n_failures=} on BSC({true_target_ch_param:.2%}). "
+    print(f"\rSimulation done. Did {n_trials=} and {n_failures=} on BSC({true_target_qber:.2%}). "
           f"FER ~ {n_failures / n_trials:.2E}")
 
 
-def test_with_block_splitting(ch_param = 0.049):
+def test_with_block_splitting(ch_param=0.049):
     ecc_code_spec = ldpc.ECCodeSpec.select_suitable(ch_param_estimate=ch_param)
 
     code = ecc_code_spec.get_corresponding_code()
@@ -192,7 +195,9 @@ def test_with_block_splitting(ch_param = 0.049):
 
 
 if __name__ == "__main__":
-    print_available_codes()
+    test_print_version()
+
+    n_codes = print_available_codes()
 
     test_small()
     test_small_default()
@@ -201,4 +206,5 @@ if __name__ == "__main__":
     for _ in range(10):
         [test_with_block_splitting(q / 1000) for q in range(5, 90, 10)]
 
-    test_big()
+    for i in range(n_codes):
+        test_big(i)
